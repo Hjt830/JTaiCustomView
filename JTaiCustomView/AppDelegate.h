d9b493bacccb503450914f004e51891f8876ec83
黄金台 <Hjt_830@163.com>
2016-03-06 22:28:02 +0800

    Initial Commit

---
+++ JTaiCustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTaiCustomView
+//
+//  Created by 黄金台 on 16/3/6.
+//  Copyright © 2016年 com.Hjt_830.163. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTaiCustomViewTests/JTaiCustomViewTests.m
@@ -0,0 +1,39 @@
+//
+//  JTaiCustomViewTests.m
+//  JTaiCustomViewTests
+//
+//  Created by 黄金台 on 16/3/6.
+//  Copyright © 2016年 com.Hjt_830.163. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JTaiCustomViewTests : XCTestCase
+
+@end
+
+@implementation JTaiCustomViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
